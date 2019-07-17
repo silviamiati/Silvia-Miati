6f4747e87c38844ce60bc45cecc842c0b32837f1
silviamiati <50879635+silviamiati@users.noreply.github.com>
2019-07-17 11:39:03 +0200

    Add files via upload

---
+++ Esercizio1_3/funzione.h
@@ -0,0 +1,11 @@
+#include <cmath>
+#include <string>
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include "random.h"
+
+void isto(int *NB, int bin, double m, double M, double R);
+void null(int *NB, int bin);
